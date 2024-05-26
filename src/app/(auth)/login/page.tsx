96294b3031438a3bae7a29832e69ce0aa20a9003
ZnovandaP <znovandaputra@gmail.com>
2024-05-26 22:33:57 +0700

    feat(app: page): add home page as initiate app

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+
+export default function Login() {
+  return (
+    <section className="">
+      <h1 className="text-3xl font-bold">
+        Hello World Login Page
+      </h1>
+
+      <Button type="button" variant="default">
+        Click me
+      </Button>
+      <Button type="button" variant="default">
+        Hallo
+      </Button>
+    </section>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Button } from '@/components/ui/button';
+
+export default function Register() {
+  return (
+    <section className="">
+      <h1 className="text-3xl font-bold">
+        Hello World Register Page
+      </h1>
+
+      <Button type="button" variant="default">
+        Click me
+      </Button>
+      <Button type="button" variant="default">
+        Hallo
+      </Button>
+    </section>
+  );
+}
