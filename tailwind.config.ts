27cf7068187263d62dab231fb6cd0e85c39cdf55
ZnovandaP <znovandaputra@gmail.com>
2024-05-26 10:35:35 +0700

    chore(tailwind.config): add some config such as color, screens etc

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,62 @@
-import type { Config } from 'tailwindcss'
+/* eslint-disable global-require */
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    screens: {
+      sm: '600px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1150px',
+      '2xl': '1280px',
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'radial-gradient': 'radial-gradient(circle at -200%, #09141a 5% , #0d1d23 20% , #1f4247 )',
+      },
+      colors: {
+        main: '#09141a',
+        second: '#0d1d23',
+        third: '#1f4247',
+      },
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    require('tailwindcss-animate'),
+    require('tailwind-scrollbar'),
+  ],
+} satisfies Config;
+
+export default config;
