7095d1017d2d270a43981617e956d22a91e37d96
ZnovandaP <znovandaputra@gmail.com>
2024-05-26 22:31:51 +0700

    feat(component): create component AppShell

---
+++ src/components/AppShell/index.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import * as React from 'react';
+import NextTopLoader from 'nextjs-toploader';
+import Main from './Main';
+import Navbar from '../Navbar';
+import Footer from './Footer';
+import Providers from '../Providers';
+
+type AppShellProps = {
+  children: React.ReactNode
+};
+
+export default function AppShell({ children }: AppShellProps) {
+  return (
+    <Providers>
+      <NextTopLoader color="#10b981" />
+      <header className="inline">
+        <Navbar />
+      </header>
+
+      <Main>
+        {children}
+      </Main>
+
+      <Footer />
+    </Providers>
+  );
+}
