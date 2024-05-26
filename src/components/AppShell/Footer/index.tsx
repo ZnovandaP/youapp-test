f979672d487527b8a854d8161bfbd81a345d6b5b
ZnovandaP <znovandaputra@gmail.com>
2024-05-26 22:31:33 +0700

    feat(component: Footer): add event onclick on Footer Component

--- src/components/AppShell/Footer/index.tsx
+++ src/components/AppShell/Footer/index.tsx
@@ -1,12 +1,26 @@
+'use client';
+
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import * as React from 'react';
+import Container from '@/components/Container';
+import useOpenNavMenu from '@/hooks/useOpenNavMenu';
 import LeftSide from './ComponentSupport/LeftSide';
 import RightSide from './ComponentSupport/RightSide';
 
 export default function Footer() {
+  const { dispatch } = useOpenNavMenu();
+
   return (
-    <section className="flex flex-col lg:flex-row gap-y-8 gap-x-4 justify-between">
-      <LeftSide />
-      <RightSide />
-    </section>
+    <footer className="bg-main p-8" onClick={() => dispatch({ type: 'close' })}>
+      <Container className="p-0">
+        <section className="flex flex-col lg:flex-row gap-y-8 gap-x-4 justify-between">
+          <LeftSide />
+          <RightSide />
+        </section>
+      </Container>
+    </footer>
+
   );
 }
