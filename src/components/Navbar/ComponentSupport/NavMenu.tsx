9852c48635551f50d3fe3917db50f24f250e10d2
ZnovandaP <znovandaputra@gmail.com>
2024-05-29 00:50:25 +0700

    feat(component: Navbar): add feature avatar on navbar when user is authenticated

--- src/components/Navbar/ComponentSupport/NavMenu.tsx
+++ src/components/Navbar/ComponentSupport/NavMenu.tsx
@@ -1,25 +1,72 @@
+'use client';
+
 import * as React from 'react';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { signIn, useSession, signOut } from 'next-auth/react';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import generateImageUserAvatar from '@/lib/utils/generate-avatar-image';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
 type NavMenuProps = {
   className?: string
 };
 
 export default function NavMenu({ className = '' }: NavMenuProps) {
-  return (
+  const { status, data } = useSession();
+
+  return status === 'authenticated' ? (
+    <Popover>
+      <PopoverTrigger>
+        <Avatar>
+          <AvatarImage
+            src={generateImageUserAvatar(data?.user?.username || data?.user?.email)}
+            alt={`Avatar user: ${data?.user?.username || data?.user?.email}`}
+          />
+          <AvatarFallback>
+            {data?.user?.username.slice(0, 2).toUpperCase()
+        || data?.user?.email.slice(0, 2).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      </PopoverTrigger>
+      <PopoverContent className="mr-4 mt-3">
+        <Button
+          className="w-full"
+          type="button"
+          variant="destructive"
+          onClick={() => signOut()}
+        >
+          Logout
+        </Button>
+      </PopoverContent>
+    </Popover>
+
+  ) : (
     <ul className={cn(
       'flex flex-col md:flex-row md:justify-end md:items-center gap-4',
       className,
     )}
     >
       <li>
-        <Button variant="outline" className="w-full" size="md">
-          Register
+        <Button
+          variant="outline"
+          className="w-full"
+          size="md"
+          asChild
+        >
+          <Link href="/register">
+            Register
+          </Link>
         </Button>
       </li>
       <li>
-        <Button variant="fancy" className="w-full" size="md">
+        <Button
+          variant="fancy"
+          onClick={() => signIn()}
+          className="w-full"
+          size="md"
+        >
           Login
         </Button>
       </li>
--- src/components/Navbar/ComponentSupport/NavMenuMobile.tsx
+++ src/components/Navbar/ComponentSupport/NavMenuMobile.tsx
@@ -2,19 +2,21 @@ import * as React from 'react';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Divide as Hamburger } from 'hamburger-react';
 import useOpenNavMenu from '@/hooks/useOpenNavMenu';
+import { useSession } from 'next-auth/react';
 import NavMenu from './NavMenu';
 
 export default function NavMenuMobile() {
   const { isOpen, dispatch } = useOpenNavMenu();
+  const { status } = useSession();
 
-  return (
+  return status === 'unauthenticated' && (
     <Popover open={isOpen}>
       <PopoverTrigger>
         <div className="block md:hidden">
           <Hamburger toggle={() => dispatch({ type: 'toggle' })} size={28} toggled={isOpen} />
         </div>
       </PopoverTrigger>
-      <PopoverContent className="mr-4 mt-8 md:hidden">
+      <PopoverContent className="mr-4 mt-3 md:hidden">
         <NavMenu />
       </PopoverContent>
     </Popover>
