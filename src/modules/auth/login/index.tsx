b4ac3ddef6cf6b457f5ba393994f46ee6b7a239b
ZnovandaP <znovandaputra@gmail.com>
2024-05-29 00:55:43 +0700

    feat(modules: login & register): mount AuthWrapper Component

--- src/modules/auth/login/index.tsx
+++ src/modules/auth/login/index.tsx
@@ -1,29 +1,11 @@
-'use client';
-
 import * as React from 'react';
-import { Button } from '@/components/ui/button';
-import { LuArrowLeft } from 'react-icons/lu';
-import { useRouter } from 'next/navigation';
 import FormLogin from '../components/FormLogin';
+import AuthWrapper from '../components/AuthWrapper';
 
 export default function Login() {
-  const { push } = useRouter();
-
   return (
-    <section className="">
-      <div>
-        <Button
-          variant="ghost"
-          size="sm"
-          className="gap-1 p-0 hover:p-3 transition-all duration-300"
-          onClick={() => push('/')}
-        >
-          <LuArrowLeft className="text-xl" />
-          Back
-        </Button>
-      </div>
-
+    <AuthWrapper>
       <FormLogin />
-    </section>
+    </AuthWrapper>
   );
 }
--- src/modules/auth/register/index.tsx
+++ src/modules/auth/register/index.tsx
@@ -1,29 +1,11 @@
-'use client';
-
 import * as React from 'react';
-import { Button } from '@/components/ui/button';
-import { LuArrowLeft } from 'react-icons/lu';
-import { useRouter } from 'next/navigation';
 import FormRegister from '../components/FormRegister';
+import AuthWrapper from '../components/AuthWrapper';
 
 export default function Register() {
-  const { push } = useRouter();
-
   return (
-    <section className="">
-      <div>
-        <Button
-          variant="ghost"
-          size="sm"
-          className="gap-1 p-0 hover:p-3 transition-all duration-300"
-          onClick={() => push('/')}
-        >
-          <LuArrowLeft className="text-xl" />
-          Back
-        </Button>
-      </div>
-
+    <AuthWrapper>
       <FormRegister />
-    </section>
+    </AuthWrapper>
   );
 }
